1c5e6c9fe775c58770f7d490366623b8b556030d
Ranga-Sri <113333156+Ranga-Sri@users.noreply.github.com>
2022-09-19 13:45:27 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,23 @@
+class A
+{
+public static void main(String args[])
+{
+int i=5;
+int j=2;
+int k=max(i,j)
+system.out.println("the maximum between is"+k);
+}
+public static int max(int num1,int num2)
+{
+int result;
+if(num1>num2)
+{
+result=num1;
+}
+else
+{
+result=num2;
+}
+return.result;
+}
+}
